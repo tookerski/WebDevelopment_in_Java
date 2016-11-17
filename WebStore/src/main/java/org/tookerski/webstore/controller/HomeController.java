eede5ffe6176de9e865738a2022f2913d1794f5d
tookerski <fdliudaiyuan1986@163.com>
2016-11-17 17:54:03 +0800

    UPDATE xml config file

---
+++ WebStore/src/main/java/org/tookerski/webstore/controller/HomeController.java
@@ -0,0 +1,13 @@
+package org.tookerski.webstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcome(){
+		return "index";
+	}
+}
